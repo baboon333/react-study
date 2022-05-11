83251c06a880f362efa27c97a064eda0ecbc70b7
supercode333 <gacomonkey123@gmail.com>
2022-05-11 23:10:53 +0900

    state 활용

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
-asdfsdafasdf
+  let [글제목, b] = useState(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'red', fontSize : '16px'} }
-        >개발 Blog</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ 글제목[0] } <span>좋아요</span> 0 </h4>
+        <p>5월 11일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>5월 11일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>5월 11일 발행</p>
+      </div>
     </div>
   );
 }
